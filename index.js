257061cf98eb3645cf6c5a4dc3662e233d9c5ee1
Caver1K <168340347+Caver1K@users.noreply.github.com>
2026-03-14 09:15:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,28 +2,18 @@ async function loadFolders() {
   const container = document.getElementById("folder-list");
 
   try {
-    const res = await fetch(
-      "https://api.github.com/repos/Caver1K/content.caver1k.net/contents/",
-      {
-        headers: {
-          "Accept": "application/vnd.github.v3+json"
-        }
-      }
-    );
-
-    const items = await res.json();
+    const res = await fetch("./folders.json");
+    const folders = await res.json();
 
     container.innerHTML = "";
-
-    const folders = items.filter(item => item.type === "dir");
 
     folders.forEach(folder => {
       const div = document.createElement("div");
       div.className = "folder";
-      div.textContent = folder.name;
+      div.textContent = folder;
 
       div.onclick = () => {
-        window.location.href = `https://aio.caver1k.net/${folder.name}`;
+        window.location.href = `https://aio.caver1k.net/${folder}`;
       };
 
       container.appendChild(div);
