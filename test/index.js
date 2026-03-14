84c7d490c27a662f43e8186373d32b76b4c1d0b8
Caver1K <168340347+Caver1K@users.noreply.github.com>
2026-03-14 15:06:49 +0100

    Update index.js

--- test/index.js
+++ test/index.js
@@ -118,7 +118,4 @@ document.getElementById("download-btn").addEventListener("click", () => {
     log("ERROR: " + err.message);
     console.error(err);
   });
-document.getElementById("back-btn").addEventListener("click", () => {
-  window.location.href = "/";
-});
 });
