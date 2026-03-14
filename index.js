f9b90fcaebbeea88bac72497e0f8fdceb70fff7f
Caver1K <168340347+Caver1K@users.noreply.github.com>
2026-03-14 08:38:15 +0100

    Add error handling and folder click functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ async function loadFolders() {
   const container = document.getElementById("folder-list");
 
   try {
+    // Load folder list (JSON array of folder names)
     const res = await fetch("./folders.json");
     const folders = await res.json();
 
@@ -11,9 +12,11 @@ async function loadFolders() {
       const div = document.createElement("div");
       div.className = "folder";
       div.textContent = folder;
+
       div.onclick = () => {
         window.location.href = `https://aio.caver1k.net/${folder}`;
       };
+
       container.appendChild(div);
     });
 
